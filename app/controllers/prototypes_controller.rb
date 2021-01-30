11cdd8a18a0f6c85cfcf1426f2535f8b08404f34
yuki5546 <inou0531@gmail.com>
2021-01-30 12:26:20 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,63 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :delete]
+  before_action :move_to_index, only: [:edit]
+
+  def index
+    @prototypes = Prototype.all
+  end
+  
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      redirect_to new_prototype_path
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.all
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+  
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      @prototype = Prototype.find(params[:id])
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    prototype = Prototype.find(params[:id])
+    unless current_user == prototype.user
+      redirect_to action: :index
+    end
+  end
+  
+end
+
+
+# .includes(:user).order("created_at DESC")
---
+++ db/migrate/20210122234158_add_user_id_to_prototypes.rb
@@ -0,0 +1,7 @@
+class AddUserIdToPrototypes < ActiveRecord::Migration[6.0]
+  def change
+
+  end
+end
+
+#add_column :prototypes, :user_id, :integer
